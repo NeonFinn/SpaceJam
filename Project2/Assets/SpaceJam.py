02c5cb6f9cd74212e4ee6e89987b2cdc4f2ea431
NeonFinn <n3onfinn@gmail.com>
2025-06-12 14:01:09 -0400

    -

--- Project2/Assets/SpaceJam.py
+++ Project2/Assets/SpaceJam.py
@@ -1,47 +1,48 @@
 from direct.showbase.ShowBase import ShowBase
+import Classes as Classes
+import DefensePaths as defensePaths
 
 class MyApp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
 
         def SetupScene():
-            self.universe = self.loader.loadModel('Universe/Universe.x')
-            self.universe.reparentTo(self.render)
-            self.universe.setScale(15000)
-            tex = self.loader.loadTexture('Universe/starfield-in-blue.jpg')
-            self.universe.setTexture(tex, 1)
 
-            self.Planet1 = self.loader.loadModel('Planets/protoPlanet.x')
-            self.Planet1.reparentTo(self.render)
-            self.Planet1.setPos(1800, 1500, -1500)  # randomize planet position later
-            self.Planet1.setScale(350)
+            self.Universe = Classes.Universe(self.loader, 'Universe/Universe.x', self.render, 'Universe', 'Universe/starfield-in-blue.jpg', (0, 0, 0), 10000)
 
-            self.Planet2 = self.loader.loadModel('Planets/protoPlanet.x')
-            self.Planet2.reparentTo(self.render)
-            self.Planet2.setPos(-1900, 4950, 1700)  # randomize planet position later
-            self.Planet2.setScale(350)
+            self.Planet1 = Classes.Planet(self.loader, 'Planets/protoPlanet.x', self.render, 'Planet1', 'Planets/planet-texture.jpg', (-6000, -3000, -800), 250)
+            self.Planet2 = Classes.Planet(self.loader, 'Planets/protoPlanet.x', self.render, 'Planet2', 'Planets/planet-texture.jpg', (0, 6000, 0), 300)
+            self.Planet3 = Classes.Planet(self.loader, 'Planets/protoPlanet.x', self.render, 'Planet3', 'Planets/planet-texture.jpg', (500, -5000, 200), 500)
+            self.Planet4 = Classes.Planet(self.loader, 'Planets/protoPlanet.x', self.render, 'Planet4', 'Planets/planet-texture.jpg', (300, 6000, 500), 150)
+            self.Planet5 = Classes.Planet(self.loader, 'Planets/protoPlanet.x', self.render, 'Planet5', 'Planets/planet-texture.jpg', (700, -2000, 100), 500)
+            self.Planet6 = Classes.Planet(self.loader, 'Planets/protoPlanet.x', self.render, 'Planet6', 'Planets/planet-texture.jpg', (0, -900, -1400), 700)
 
-            self.Planet3 = self.loader.loadModel('Planets/protoPlanet.x')
-            self.Planet3.reparentTo(self.render)
-            self.Planet3.setPos(900, 5300, 2000)  # randomize planet position later
-            self.Planet3.setScale(350)
+            self.SpaceStation1 = Classes.SpaceStation(self.loader, 'SpaceStation/spaceStation.x', self.render, 'SpaceStation1', 'SpaceStation/space-station-texture.jpg', (1500, 1000, -100), 40)
+            self.Hero = Classes.SpaceShip(self.loader, 'Spaceship/heroShip.x', self.render, 'Hero', 'Spaceship/hero-ship-texture.jpg', Vec3(1000, 1200, -50), 50)
 
-            self.Planet4 = self.loader.loadModel('Planets/protoPlanet.x')
-            self.Planet4.reparentTo(self.render)
-            self.Planet4.setPos(-1200, 5000, -1800)  # randomize planet position later
-            self.Planet4.setScale(350)
+        def DrawBaseballSeams(self, centralObject, droneName, step, numSeams, radius = 1):
+            unitVec = defensePaths.BaseballSeams(step, numSeams, B = 0.4)
+            unitVec.normalize()
+            position = unitVec * radius * 250 + centralObject.modelNode.getPos()
+            Classes.Drone(self.loader, 'DroneDefender/DroneDefender.obj', self.render, droneName, 'Drone/drone-texture.jpg', position, 5)
 
-            self.Planet5 = self.loader.loadModel('Planets/protoPlanet.x')
-            self.Planet5.reparentTo(self.render)
-            self.Planet5.setPos(0, 4850, 0)  # randomize planet position later
-            self.Planet5.setScale(350)
-
-            self.Planet6 = self.loader.loadModel('Planets/protoPlanet.x')
-            self.Planet6.reparentTo(self.render)
-            self.Planet6.setPos(1600, 5200, 800)  # randomize planet position later
-            self.Planet6.setScale(350)
+        def DrawCloudDefense(self, centralObject, droneName):
+            unitVec = defensePaths.Cloud(radius = 1)
+            unitVec.normalize()
+            position = unitVec * 500 + centralObject.modelNode.getPos()
+            Classes.Drone(self.loader, 'DroneDefender/DroneDefender.obj', self.render, droneName, 'Drone/drone-texture.jpg', position, 10)
 
         SetupScene()
+
+        fullCycle = 60
+
+        for j in range(fullCycle):
+            Classes.Drone.droneCount +=1
+            droneName = "Drone" + str(Classes.Drone.droneCount)
+
+            self.DrawCloudDefense(self.Planet1, droneName)
+            self.DrawBaseballSeams(self.SpaceStation1, droneName, j, fullCycle, 2)
+
 
 app = MyApp() # create instance of MyApp
 app.run() # run application
