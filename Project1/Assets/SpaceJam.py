df551f7c27895b3082d0cee103416dd3c84b4283
NeonFinn <n3onfinn@gmail.com>
2025-06-12 14:03:58 -0400

    -

--- Project1/Assets/SpaceJam.py
+++ Project1/Assets/SpaceJam.py
@@ -15,31 +15,43 @@ class MyApp(ShowBase):
             self.Planet1.reparentTo(self.render)
             self.Planet1.setPos(1800, 1500, -1500)  # randomize planet position later
             self.Planet1.setScale(350)
+            tex = self.loader.loadTexture('Planets/Jupiter.jpg')
+            self.Planet1.setTexture(tex, 1)
 
             self.Planet2 = self.loader.loadModel('Planets/protoPlanet.x')
             self.Planet2.reparentTo(self.render)
             self.Planet2.setPos(-1900, 4950, 1700)  # randomize planet position later
             self.Planet2.setScale(350)
+            tex = self.loader.loadTexture('Planets/Mars.jpg')
+            self.Planet2.setTexture(tex, 1)
 
             self.Planet3 = self.loader.loadModel('Planets/protoPlanet.x')
             self.Planet3.reparentTo(self.render)
             self.Planet3.setPos(900, 5300, 2000)  # randomize planet position later
             self.Planet3.setScale(350)
+            tex = self.loader.loadTexture('Planets/Mercury.jpg')
+            self.Planet3.setTexture(tex, 1)
 
             self.Planet4 = self.loader.loadModel('Planets/protoPlanet.x')
             self.Planet4.reparentTo(self.render)
             self.Planet4.setPos(-1200, 5000, -1800)  # randomize planet position later
             self.Planet4.setScale(350)
+            tex = self.loader.loadTexture('Planets/Neptune.jpg')
+            self.Planet4.setTexture(tex, 1)
 
             self.Planet5 = self.loader.loadModel('Planets/protoPlanet.x')
             self.Planet5.reparentTo(self.render)
             self.Planet5.setPos(0, 4850, 0)  # randomize planet position later
             self.Planet5.setScale(350)
+            tex = self.loader.loadTexture('Planets/Uranus.jpg')
+            self.Planet5.setTexture(tex, 1)
 
             self.Planet6 = self.loader.loadModel('Planets/protoPlanet.x')
             self.Planet6.reparentTo(self.render)
             self.Planet6.setPos(1600, 5200, 800)  # randomize planet position later
             self.Planet6.setScale(350)
+            tex = self.loader.loadTexture('Planets/Venus.jpg')
+            self.Planet6.setTexture(tex, 1)
 
         SetupScene()
 
