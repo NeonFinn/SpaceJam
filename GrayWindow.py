89c93e1f9d22319e421428a63e4b2e6095165574
NeonFinn <n3onfinn@gmail.com>
2025-06-12 12:38:50 -0400

    -

--- GrayWindow.py
+++
@@ -1,8 +0,0 @@
-from direct.showbase.ShowBase import ShowBase
-
-class MyApp(ShowBase):
-    def __init__(self):
-        ShowBase.__init__(self)
-
-app = MyApp() # create instance of MyApp
-app.run() # run application
