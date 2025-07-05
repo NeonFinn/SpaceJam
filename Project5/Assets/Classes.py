583015b3454f58ce40272f0aceda0e3e8e388a53
NeonFinn <n3onfinn@gmail.com>
2025-07-05 15:06:09 -0400

    -

---
+++ Project5/Assets/Classes.py
@@ -0,0 +1,66 @@
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import *
+from direct.task import Task
+
+from CollideObjectBase import *
+
+class Planet(ShowBase):
+    def __init__(self, loader, modelPath, parentNode, nodeName, texPath, posVec, scaleVec):
+        self.modelNode = loader.loadModel(modelPath)
+        self.modelNode.reparentTo(parentNode)
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scaleVec)
+
+        self.modelNode.setName(nodeName)
+        tex = loader.loadTexture(texPath)
+        self.modelNode.setTexture(tex, 1)
+
+        self.collisionNode.node().clearSolids()
+        self.collisionNode.node().addSolid(CollisionSphere(0,0,0, 1.25))
+        self.collisionNode.show()
+
+class Drone(ShowBase):
+    droneCount = 0
+
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, texPath: str, posVec: Vec3, scaleVec: float):
+        self.modelNode = loader.loadModel(modelPath)
+        self.modelNode.reparentTo(parentNode)
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scaleVec)
+        self.modelNode.setName(nodeName)
+
+        tex = loader.loadTexture(texPath)
+        self.modelNode.setTexture(tex, 1)
+
+        self.collisionNode = self.modelNode.attachNewNode(CollisionNode(nodeName + '_cNode'))
+        self.collisionNode.node().addSolid(CollisionSphere(0, 0, 0, 5))
+        self.collisionNode.show()
+
+        Drone.droneCount += 1
+
+class SpaceStation(CapsuleCollideObject):
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str,
+                 texPath: str, posVec, scaleVec):
+        super().__init__(loader, modelPath, parentNode, nodeName, 1, -1, 5, 1, -1, -5, 10)
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scaleVec)
+        tex = loader.loadTexture(texPath)
+        self.modelNode.setTexture(tex, 1)
+
+class Universe(InverseSphereCollideObject):
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, texPath: str, posVec: Vec3, scaleVec: float):
+        super().__init__(loader, modelPath, parentNode, nodeName, Vec3(0, 0, 0), 0.9)
+
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scaleVec)
+        tex = loader.loadTexture(texPath)
+        self.modelNode.setTexture(tex, 1)
+
+class Missile(CapsuleCollideObject):
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str,
+                 texPath: str, posVec: Vec3, scaleVec: float):
+        super().__init__(loader, modelPath, parentNode, nodeName, 1, -1, 5, 1, -1, -5, 10)
+
+        self.reloadTime = 0.25
+        self.missileDistance = 4000
+        self.missileBay = 1
