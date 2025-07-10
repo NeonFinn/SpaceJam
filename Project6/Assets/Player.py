f1deaad175c2ebd819fa0efce340b2b89ba6f09e
NeonFinn <n3onfinn@gmail.com>
2025-07-10 14:02:24 -0400

    -

---
+++ Project6/Assets/Player.py
@@ -0,0 +1,231 @@
+from CollideObjectBase import SphereCollideObject
+from panda3d.core import Loader, NodePath, Vec3
+from direct.task.Task import TaskManager
+from typing import Callable
+from direct.task import Task
+from panda3d.core import CollisionNode, CollisionSphere
+from Project5.Assets import Classes
+from panda3d.core import CollisionHandlerEvent
+from direct.interval.LerpInterval import LerpFunc
+from direct.particles.ParticleEffect import ParticleEffect
+import re
+from panda3d.core import CollisionTraverser
+
+
+class player:
+    def __init__(self, loader: Loader, taskMgr, accept: Callable, modelPath: str, parentNode: NodePath,
+                 nodeName: str, texPath: str, posVec: Vec3, scaleVec: float, base):
+
+        self.base = base
+        self.render = base.render
+        self.taskMgr = taskMgr
+        self.accept = accept
+        self.cTrav = CollisionTraverser()
+
+        # Load and set up the model
+        self.modelNode = loader.loadModel(modelPath)
+        self.modelNode.reparentTo(parentNode)
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scaleVec)
+        self.modelNode.setName(nodeName)
+        tex = loader.loadTexture(texPath)
+        self.modelNode.setTexture(tex, 1)
+
+        # Set up collision
+        self.collisionNode = self.modelNode.attachNewNode(CollisionNode(nodeName + '_cNode'))
+        self.collisionNode.node().addSolid(CollisionSphere(0, 0, 0, 1))
+        self.collisionNode.show()
+
+        # Event collision handler
+        self.handler = CollisionHandlerEvent()
+        self.handler.addInPattern('into')
+        self.cTrav.addCollider(self.collisionNode, self.handler)
+
+        self.accept('into', self.HandleInto)
+
+        # Explosion effects
+        self.cntExplode = 0
+        self.explodeIntervals = {}
+
+        # Controls
+        self.keys = {
+            "forward": False, "turnLeft": False, "turnRight": False,
+            "turnDown": False, "turnUp": False,
+            "rollLeft": False, "rollRight": False, "fire": False
+        }
+        self.setKeyBinds()
+
+        # Tasks
+        self.taskMgr.add(self.updatePlayer, "updatePlayer")
+        self.taskMgr.add(self.checkIntervals, 'checkMissiles', 34)
+
+    def setKey(self, key, value):
+        self.keys[key] = value
+
+    def setKeyBinds(self):
+        self.base.accept("space", self.setKey, ["forward", True])
+        self.base.accept("space-up", self.setKey, ["forward", False])
+        self.base.accept("a", self.setKey, ["turnLeft", True])
+        self.base.accept("a-up", self.setKey, ["turnLeft", False])
+        self.base.accept("d", self.setKey, ["turnRight", True])
+        self.base.accept("d-up", self.setKey, ["turnRight", False])
+        self.base.accept("s", self.setKey, ["turnDown", True])
+        self.base.accept("s-up", self.setKey, ["turnDown", False])
+        self.base.accept("w", self.setKey, ["turnUp", True])
+        self.base.accept("w-up", self.setKey, ["turnUp", False])
+        self.base.accept("q", self.setKey, ["rollLeft", True])
+        self.base.accept("q-up", self.setKey, ["rollLeft", False])
+        self.base.accept("e", self.setKey, ["rollRight", True])
+        self.base.accept("e-up", self.setKey, ["rollRight", False])
+        self.base.accept("f", self.setKey, ["fire", True])
+
+    def updatePlayer(self, task):
+        rate = 0.25
+        if self.keys["turnLeft"]:
+            self.modelNode.setH(self.modelNode.getH() + rate)
+        if self.keys["turnRight"]:
+            self.modelNode.setH(self.modelNode.getH() - rate)
+        if self.keys["turnDown"]:
+            self.modelNode.setP(self.modelNode.getP() - rate)
+        if self.keys["turnUp"]:
+            self.modelNode.setP(self.modelNode.getP() + rate)
+        if self.keys["rollLeft"]:
+            self.modelNode.setR(self.modelNode.getR() - rate)
+        if self.keys["rollRight"]:
+            self.modelNode.setR(self.modelNode.getR() + rate)
+        if self.keys["forward"]:
+            self.applyThrust()
+        if self.keys["fire"]:
+            self.fireMissile()
+            self.keys["fire"] = False  # Reset fire key to prevent multiple firings
+
+        return Task.cont
+
+    def applyThrust(self):
+        rate = 2
+        trajectory = self.base.render.getRelativeVector(self.modelNode, Vec3(0, 1, 0))  # Forward is Y
+        trajectory.normalize()
+        self.modelNode.setFluidPos(self.modelNode.getPos() + trajectory * rate)
+
+    def fireMissile(self):
+        if Classes.Missile.missileBay > 0:
+            aim = self.base.render.getRelativeVector(self.modelNode, Vec3.forward())
+            aim.normalize()
+
+            fireSolution = aim * Classes.Missile.missileDistance
+            inFront = aim * 150
+
+            travVec = fireSolution + self.modelNode.getPos()
+            Classes.Missile.missileBay -= 1
+            tag = 'Missile' + str(Classes.Missile.missileCount + 1)
+            Classes.Missile.missileCount += 1
+
+            posVec = self.modelNode.getPos() + inFront
+            currentMissile = Classes.Missile(self.base.loader, 'Phaser/phaser.egg', self.base.render,
+                                             tag, posVec, 4.0)
+
+            Classes.Missile.intervals[tag] = currentMissile.modelNode.posInterval(
+                2.0, travVec, startPos=posVec, fluid=1)
+
+            Classes.Missile.intervals[tag].start()
+
+            self.isReloading = False
+
+            self.cTrav.addCollider(currentMissile.collisionNode, self.handler)
+
+        else:
+            if not self.taskMgr.hasTaskNamed('missileReload'):
+                print('Initializing reload...')
+                self.isReloading = False
+                self.taskMgr.doMethodLater(0, self.reload, 'reload')
+
+                return Task.cont
+
+    def reload(self, task):
+        if not self.isReloading:
+            print('Reloading...')  # print once at the start
+            self.isReloading = True
+
+        if task.time > Classes.Missile.reloadTime:
+            if Classes.Missile.missileBay > 1:
+                Classes.Missile.missileBay = 1
+            print('Reload complete.')
+            self.isReloading = False
+            return Task.done
+
+        return Task.cont
+
+    def checkIntervals(self, task):
+        for i in Classes.Missile.intervals:
+            if not Classes.Missile.intervals[i].isPlaying(): # returns true or false to see if missile has reached path end
+                Classes.Missile.cNodes[i].detachNode()
+                Classes.Missile.fireModels[i].detachNode()
+
+                del Classes.Missile.intervals[i]
+                del Classes.Missile.fireModels[i]
+                del Classes.Missile.cNodes[i]
+                del Classes.Missile.collisionSolids[i]
+
+                Classes.Missile.missileBay += 1
+                print(i + ' has reached the end of its fire solution.')
+
+                break # refactoring to remove all intervals that have completed their fire solution
+
+        return Task.cont
+
+    def HandleInto(self, entry):
+        fromNode = entry.getFromNodePath().getName()
+        print("fromNode: " + fromNode)
+        intoNode = entry.getIntoNodePath().getName()
+        print("intoNode: " + intoNode)
+
+        intoPosition = Vec3(entry.getSurfacepoints(self.render))
+
+        tempVar = fromNode.split('_')
+        print("tempVar: " + str(tempVar))
+        shooter = tempVar[0]
+        print("Shooter: " + str(shooter))
+        tempVar = intoNode.split('-')
+        print("TempVar1: " + str(tempVar))
+        tempVar = intoNode.split('_')
+        print("TempVar2: " + str(tempVar))
+        victim = tempVar[0]
+        print("Victim: " + str(victim))
+
+        pattern = r'[0-9]'
+        strippedString = re.sub(pattern, '', victim)
+
+        if (strippedString == "Drone" or strippedString == "Planet" or strippedString == "Space Station"):
+            print(victim, ' hit at ', intoPosition)
+            self.ObjectDestroy(intoNode, intoPosition)
+
+        print(shooter + ' is DONE')
+        Classes.Missile.intervals[shooter].finish()
+
+    def ObjectDestroy(self, hitID, hitPosition):
+        nodeID = self.render.find(hitID)
+        nodeID.detachNode()
+
+        self.explodeNode.setPos(hitPosition)
+        self.Explode()
+
+    def Explode(self):
+        self.cntExplode += 1
+        tag = 'particles_' + str(self.cntExplode)
+
+        self.explodeIntervals[tag] = LerpFunc(self.ExplodeLight, duration = 4.0)
+        self.explodeIntervals[tag].start()
+
+    def ExplodeLight(self, t):
+        if t == 1.0 and self.explodeEffect:
+            self.explodeEffect.disable()
+
+        elif t == 0.0 and self.explodeEffect:
+            self.explodeEffect.start(self.explodeNode)
+
+    def SetParticles(self):
+        # self.enableParticles()
+        self.explodeEffect = ParticleEffect()
+        self.explodeEffect.load_config('Part-Efx/basic_xpld_efx.ptf')
+        self.explodeEffect.setScale(20)
+        self.explodeNode = self.render.attachNewNode('ExplosionEffects')
---
+++ Project6/Assets/SpaceJam.py
@@ -0,0 +1,156 @@
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import *
+from panda3d.core import CollisionTraverser, CollisionHandlerPusher
+from direct.gui.OnscreenImage import OnscreenImage
+
+import Classes as Classes
+import DefensePaths as defensePaths
+import CollideObjectBase
+import Player as Player
+
+class MyApp(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        def SetupScene():
+            self.Universe = Classes.Universe(self.loader, 'Universe/Universe.x', self.render, 'Universe',
+                                             'Universe/starfield-in-blue.jpg', Vec3(0, 0, 0), 10000)
+
+            self.Planet1 = CollideObjectBase.SphereCollideObject(self.loader, 'Planets/protoPlanet.x', self.render,'Planet1',
+                'Planets/Jupiter.jpg', (-6000, -3000, -800), 250, colRadius= 1.1)
+            self.Planet2 = CollideObjectBase.SphereCollideObject(self.loader, 'Planets/protoPlanet.x', self.render,'Planet2',
+                'Planets/Mars.jpg', (800, 6000, 0), 300, colRadius= 1.1)
+            self.Planet3 = CollideObjectBase.SphereCollideObject(self.loader, 'Planets/protoPlanet.x', self.render,'Planet3',
+                'Planets/Mercury.jpg', (5500, -5000, 1000), 500, colRadius= 1.1)
+            self.Planet4 = CollideObjectBase.SphereCollideObject(self.loader, 'Planets/protoPlanet.x', self.render,'Planet4',
+                'Planets/Neptune.jpg', (-1200, 6000, 500), 150, colRadius= 1.1)
+            self.Planet5 = CollideObjectBase.SphereCollideObject(self.loader, 'Planets/protoPlanet.x', self.render,'Planet5',
+                'Planets/Uranus.jpg', (-5000, 3000, -4000), 500, colRadius= 1.1)
+            self.Planet6 = CollideObjectBase.SphereCollideObject(self.loader, 'Planets/protoPlanet.x', self.render,'Planet6',
+                'Planets/Venus.jpg', (4000, -1300, -1400), 300, colRadius= 1.1)
+
+            self.SpaceStation1 = Classes.SpaceStation(self.loader, 'SpaceStation/spaceStation.x', self.render,'SpaceStation1',
+                'SpaceStation/SpaceStation1_Dif2.png', (-2500, 1000, -100),30)
+
+            self.Player = Player.player(self.loader, self.taskMgr, self.accept, 'Spaceships/Dumbledore.x', self.render,
+                'Player', 'Spaceships/spacejet_C.png', (0, 0, 0), 3.0, self)
+
+            self.cTrav = CollisionTraverser()
+            self.pusher = CollisionHandlerPusher()
+
+            self.pusher.addCollider(self.Player.collisionNode, self.Player.modelNode)
+            self.cTrav.addCollider(self.Player.collisionNode, self.pusher)
+
+            for planet in [self.Planet1, self.Planet2, self.Planet3, self.Planet4, self.Planet5, self.Planet6]:
+                self.pusher.addCollider(planet.collisionNode, planet.modelNode)
+                self.cTrav.addCollider(planet.collisionNode, self.pusher)
+
+            self.Player.modelNode.setHpr(0, 0, 0)
+            self.cloudDrones = []
+
+        SetupScene()
+        self.enableHud()
+        self.taskMgr.add(self.SpawnDrones, 'SpawnDronesTask')
+        self.setCamera()
+
+    def setCamera(self):
+        self.disable_mouse()
+        self.camera.reparentTo(self.Player.modelNode)
+        self.camera.setFluidPos(0, 0, 0)
+
+    def DrawBaseballSeams(self, centralObject, droneName, step, numSeams, radius = 1):
+        unitVec = defensePaths.BaseballSeams(step, numSeams, B = 0.4)
+        unitVec.normalize()
+        position = unitVec * radius * 250 + centralObject.modelNode.getPos()
+
+        drone = Classes.Drone(self.loader, 'DroneDefender/DroneDefender.x', self.render,
+                              droneName, 'DroneDefender/Drones.jpg', position, 5)
+        self.cloudDrones.append(drone)
+
+    def DrawCloudDefense(self, centralObject, droneName):
+        unitVec = defensePaths.Cloud(radius=1)
+        unitVec.normalize()
+        position = unitVec * 700 + centralObject.modelNode.getPos()
+
+        drone = Classes.Drone(self.loader, 'DroneDefender/DroneDefender.x', self.render,
+                              droneName, 'DroneDefender/Drones.jpg', position, 5)
+        self.cloudDrones.append(drone)
+
+    def DrawCircleX(self, droneName, radius = 1, numPoints = 100, step = 50):
+        points = defensePaths.CircleX(radius, numPoints)
+        if step < len(points):
+            unitVec = points[step]
+            position = unitVec * 300
+
+            drone = Classes.Drone(self.loader, 'DroneDefender/DroneDefender.x', self.render,
+                                  droneName, 'DroneDefender/Drones.jpg', position, 5)
+            self.cloudDrones.append(drone)
+
+
+    def DrawCircleY(self, droneName, radius = 1, numPoints = 100, step = 50):
+        points = defensePaths.CircleY(radius, numPoints)
+        if step < len(points):
+            unitVec = points[step]
+            position = unitVec * 300
+
+            drone = Classes.Drone(self.loader, 'DroneDefender/DroneDefender.x', self.render,
+                                  droneName, 'DroneDefender/Drones.jpg', position, 5)
+            self.cloudDrones.append(drone)
+
+
+    def DrawCircleZ(self, droneName, radius = 1, numPoints = 100, step = 50):
+        points = defensePaths.CircleZ(radius, numPoints)
+        if step < len(points):
+            unitVec = points[step]
+            position = unitVec * 300
+
+            drone = Classes.Drone(self.loader, 'DroneDefender/DroneDefender.x', self.render,
+                                  droneName, 'DroneDefender/Drones.jpg', position, 5)
+            self.cloudDrones.append(drone)
+
+    def SpawnDrones(self, task):
+
+        if task.frame == 0:
+            for i in range(60):
+                self.DrawCircleX(droneName=f'DroneX_{i}', radius=3, numPoints=60, step=i)
+                self.cloudDrones[-1].modelNode.setColor(1, 0, 0, 1)
+
+                self.DrawCircleY(droneName=f'DroneY_{i}', radius=3, numPoints=60, step=i)
+                self.cloudDrones[-1].modelNode.setColor(0, 1, 0, 1)
+
+                self.DrawCircleZ(droneName=f'DroneZ_{i}', radius=3, numPoints=60, step=i)
+                self.cloudDrones[-1].modelNode.setColor(0, 0, 1, 1)
+
+            for i in range(60):
+                droneName = f'BaseballSeam_{i}'
+                self.DrawBaseballSeams(self.SpaceStation1, droneName, i, numSeams=60)
+
+        maxCloudDrones = 400
+        if len(self.cloudDrones) >= maxCloudDrones:
+            return task.cont
+
+        while len(self.cloudDrones) < maxCloudDrones:
+            Classes.Drone.droneCount += 1
+            droneName = f'Drone{Classes.Drone.droneCount}'
+
+            unitVec = defensePaths.Cloud(radius=1)
+            unitVec.normalize()
+            position = unitVec * 350 + self.Planet4.modelNode.getPos()
+
+            drone = Classes.Drone(self.loader, 'DroneDefender/DroneDefender.x', self.render,
+                          droneName, 'DroneDefender/Drones.jpg', position, 5)
+
+            self.cloudDrones.append(drone)
+            self.pusher.addCollider(drone.collisionNode, drone.modelNode)
+            self.cTrav.addCollider(drone.collisionNode, self.pusher)
+
+        return task.cont
+
+    def enableHud(self):
+        self.Hud = OnscreenImage(image ="Hud/crosshair.png", pos = Vec3(0, 0, 0), scale = (0.1))
+        self.Hud.setTransparency(TransparencyAttrib.MAlpha)
+
+
+
+app = MyApp() # create instance of MyApp
+app.run() # run application
