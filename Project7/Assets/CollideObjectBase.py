95e1aaee50911ab7be11dcdf46484afbd407965c
NeonFinn <n3onfinn@gmail.com>
2025-07-20 19:43:09 -0400

    -

---
+++ Project7/Assets/CollideObjectBase.py
@@ -0,0 +1,52 @@
+from panda3d.core import PandaNode, Loader, NodePath, CollisionNode, CollisionSphere, CollisionInvSphere, CollisionCapsule, Vec3
+
+class PlacedObject(PandaNode):
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str):
+
+        self.modelNode = loader.loadModel(modelPath)
+
+        if not isinstance(self.modelNode, NodePath):
+            raise AssertionError("PlacedObject loader.loadModel( " + modelPath + ") did not return a proper PandaNode")
+
+        self.modelNode.reparentTo(parentNode)
+        self.modelNode.setName(nodeName)
+
+class CollideableObject(PlacedObject):
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str):
+        super(CollideableObject, self).__init__(loader, modelPath, parentNode, nodeName)
+
+        self.collisionNode = self.modelNode.attachNewNode(CollisionNode(nodeName + '_cNode'))
+
+class InverseSphereCollideObject(CollideableObject):
+    def __init__(self, loader, modelPath, parentNode, nodeName, colPositionVec, colRadius):
+        super(InverseSphereCollideObject, self).__init__(loader, modelPath, parentNode, nodeName)
+
+        self.collisionNode.node().addSolid(CollisionInvSphere(colPositionVec, colRadius))
+
+class SphereCollideObject(CollideableObject):
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str,
+                 texPath: str, posVec: Vec3, scale: float,
+                 colPosVec: Vec3 = Vec3(0, 0, 0), colRadius: float = 1.0) -> None:
+        super(SphereCollideObject, self).__init__(loader, modelPath, parentNode, nodeName)
+
+        self.collisionNode.node().addSolid(CollisionSphere(colPosVec, colRadius))
+        self.collisionNode.show()
+
+        if isinstance(texPath, str):
+            tex = loader.loadTexture(texPath)
+            self.modelNode.setTexture(tex, 1)
+
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scale)
+
+class CapsuleCollideObject:
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str,
+                 ax: float, ay: float, az: float, bx: float, by: float, bz: float, r: float):
+        self.modelNode = loader.loadModel(modelPath)
+        self.modelNode.reparentTo(parentNode)
+        self.modelNode.setName(nodeName)
+        capsule = CollisionCapsule(ax, ay, az, bx, by, bz, r)
+        self.collisionNode = CollisionNode(nodeName + '_cNode')
+        self.collisionNode.addSolid(capsule)
+        self.collisionNodePath = self.modelNode.attachNewNode(self.collisionNode)
+        self.collisionNodePath.show()
---
+++ Project7/Assets/SpaceJam.py
@@ -0,0 +1,170 @@
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import *
+from panda3d.core import CollisionTraverser, CollisionHandlerPusher
+from direct.gui.OnscreenImage import OnscreenImage
+from panda3d.physics import PhysicsManager, ParticleSystemManager
+
+import Classes as classesRef
+import DefensePaths as defensePaths
+import CollideObjectBase
+import Player
+
+class MyApp(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        # Initialize physics manager
+        self.physicsMgr = PhysicsManager()
+        base.physicsMgr = self.physicsMgr
+
+        # Initialize particle manager
+        self.particleMgr = ParticleSystemManager()
+        base.particleMgr = self.particleMgr
+
+        self.enableParticles() # Call function to actually let particles show up... I forgot last time
+
+        def SetupScene():
+            self.Universe = classesRef.Universe(self.loader, 'Universe/Universe.x', self.render, 'Universe',
+                                             'Universe/starfield-in-blue.jpg', Vec3(0, 0, 0), 10000)
+
+            self.Planet1 = CollideObjectBase.SphereCollideObject(self.loader, 'Planets/protoPlanet.x', self.render,'Planet1',
+                'Planets/Jupiter.jpg', (-6000, -3000, -800), 250, colRadius= 1.1)
+            self.Planet2 = CollideObjectBase.SphereCollideObject(self.loader, 'Planets/protoPlanet.x', self.render,'Planet2',
+                'Planets/Mars.jpg', (800, 6000, -700), 300, colRadius= 1.1)
+            self.Planet3 = CollideObjectBase.SphereCollideObject(self.loader, 'Planets/protoPlanet.x', self.render,'Planet3',
+                'Planets/Mercury.jpg', (1000, -8000, 1000), 500, colRadius= 1.1)
+            self.Planet4 = CollideObjectBase.SphereCollideObject(self.loader, 'Planets/protoPlanet.x', self.render,'Planet4',
+                'Planets/Neptune.jpg', (-1200, 6000, 500), 150, colRadius= 1.1)
+            self.Planet5 = CollideObjectBase.SphereCollideObject(self.loader, 'Planets/protoPlanet.x', self.render,'Planet5',
+                'Planets/Uranus.jpg', (-5000, 8000, -2000), 500, colRadius= 1.1)
+            self.Planet6 = CollideObjectBase.SphereCollideObject(self.loader, 'Planets/protoPlanet.x', self.render,'Planet6',
+                'Planets/Venus.jpg', (4000, -2300, -1400), 300, colRadius= 1.1)
+
+            self.SpaceStation1 = classesRef.SpaceStation(self.loader, 'SpaceStation/spaceStation.x', self.render,'SpaceStation1',
+                'SpaceStation/SpaceStation1_Dif2.png', (-2500, 1000, -100),30)
+
+            self.fogZone = classesRef.FogZone(self.render, Vec3(2000, 2000, 200), 500)
+
+            self.cTrav = CollisionTraverser()
+            self.pusher = CollisionHandlerPusher()
+
+            self.Player = Player.player(self.loader, self.taskMgr, self.accept, 'Spaceships/Dumbledore.x', self.render,
+                                        'Player', 'Spaceships/spacejet_C.png', (0, 0, 0), 3.0, self)
+
+            self.pusher.addCollider(self.Player.collisionNode, self.Player.modelNode)
+            self.cTrav.addCollider(self.Player.collisionNode, self.pusher)
+
+            for planet in [self.Planet1, self.Planet2, self.Planet3, self.Planet4, self.Planet5, self.Planet6]:
+                self.pusher.addCollider(planet.collisionNode, planet.modelNode)
+                self.cTrav.addCollider(planet.collisionNode, self.pusher)
+
+            self.Player.modelNode.setHpr(0, 0, 0)
+            self.cloudDrones = []
+
+            self.Sentinal1 = classesRef.Orbiter(self.loader, self.taskMgr, self.rootAssetFolder + "/DroneDefender/DroneDefender/obj", self.render,
+                                             "Drone", 6.0, self.rootAssetFolder + "/DroneDefender/octotoad1_auv.png", self.Planet5, 900, "MLB", self.Player)
+            self.Sentinal2 = classesRef.Orbiter(self.loader, self.taskMgr,self.rootAssetFolder + "/DroneDefender/DroneDefender/obj", self.render,
+                                             "Drone", 6.0, self.rootAssetFolder + "/DroneDefender/octotoad1_auv.png",self.Planet2, 500, "Cloud", self.Player)
+
+        SetupScene()
+        self.enableHud()
+        self.taskMgr.add(self.SpawnDrones, 'SpawnDronesTask')
+        self.setCamera()
+
+    def setCamera(self):
+        self.disable_mouse()
+        self.camera.reparentTo(self.Player.modelNode)
+        self.camera.setFluidPos(0, 0, 0)
+
+    def DrawBaseballSeams(self, centralObject, droneName, step, numSeams, radius = 1):
+        unitVec = defensePaths.BaseballSeams(step, numSeams, B = 0.4)
+        unitVec.normalize()
+        position = unitVec * radius * 250 + centralObject.modelNode.getPos()
+
+        drone = classesRef.Drone(self.loader, 'DroneDefender/DroneDefender.x', self.render,
+                              droneName, 'DroneDefender/Drones.jpg', position, 5)
+        self.cloudDrones.append(drone)
+
+    def DrawCloudDefense(self, centralObject, droneName):
+        unitVec = defensePaths.Cloud(radius=1)
+        unitVec.normalize()
+        position = unitVec * 700 + centralObject.modelNode.getPos()
+
+        drone = classesRef.Drone(self.loader, 'DroneDefender/DroneDefender.x', self.render,
+                              droneName, 'DroneDefender/Drones.jpg', position, 5)
+        self.cloudDrones.append(drone)
+
+    def DrawCircleX(self, droneName, radius = 1, numPoints = 100, step = 50):
+        points = defensePaths.CircleX(radius, numPoints)
+        if step < len(points):
+            unitVec = points[step]
+            position = unitVec * 300
+
+            drone = classesRef.Drone(self.loader, 'DroneDefender/DroneDefender.x', self.render,
+                                  droneName, 'DroneDefender/Drones.jpg', position, 5)
+            self.cloudDrones.append(drone)
+
+    def DrawCircleY(self, droneName, radius = 1, numPoints = 100, step = 50):
+        points = defensePaths.CircleY(radius, numPoints)
+        if step < len(points):
+            unitVec = points[step]
+            position = unitVec * 300
+
+            drone = classesRef.Drone(self.loader, 'DroneDefender/DroneDefender.x', self.render,
+                                  droneName, 'DroneDefender/Drones.jpg', position, 5)
+            self.cloudDrones.append(drone)
+
+
+    def DrawCircleZ(self, droneName, radius = 1, numPoints = 100, step = 50):
+        points = defensePaths.CircleZ(radius, numPoints)
+        if step < len(points):
+            unitVec = points[step]
+            position = unitVec * 300
+
+            drone = classesRef.Drone(self.loader, 'DroneDefender/DroneDefender.x', self.render,
+                                  droneName, 'DroneDefender/Drones.jpg', position, 5)
+            self.cloudDrones.append(drone)
+
+    def SpawnDrones(self, task):
+        if task.frame == 0:
+            for i in range(60):
+                self.DrawCircleX(droneName=f'DroneX_{i}', radius=3, numPoints=60, step=i)
+                self.cloudDrones[-1].modelNode.setColor(1, 0, 0, 1)
+
+                self.DrawCircleY(droneName=f'DroneY_{i}', radius=3, numPoints=60, step=i)
+                self.cloudDrones[-1].modelNode.setColor(0, 1, 0, 1)
+
+                self.DrawCircleZ(droneName=f'DroneZ_{i}', radius=3, numPoints=60, step=i)
+                self.cloudDrones[-1].modelNode.setColor(0, 0, 1, 1)
+
+            for i in range(60):
+                droneName = f'BaseballSeam_{i}'
+                self.DrawBaseballSeams(self.SpaceStation1, droneName, i, numSeams=60)
+
+        maxCloudDrones = 400
+        if len(self.cloudDrones) >= maxCloudDrones:
+            return task.cont
+
+        while len(self.cloudDrones) < maxCloudDrones:
+            classesRef.Drone.droneCount += 1
+            droneName = f'Drone{classesRef.Drone.droneCount}'
+
+            unitVec = defensePaths.Cloud(radius=1)
+            unitVec.normalize()
+            position = unitVec * 350 + self.Planet4.modelNode.getPos()
+
+            drone = classesRef.Drone(self.loader, 'DroneDefender/DroneDefender.x', self.render,
+                          droneName, 'DroneDefender/Drones.jpg', position, 5)
+
+            self.cloudDrones.append(drone)
+            self.pusher.addCollider(drone.collisionNode, drone.modelNode)
+            self.cTrav.addCollider(drone.collisionNode, self.pusher)
+
+        return task.cont
+
+    def enableHud(self):
+        self.Hud = OnscreenImage(image ="Hud/crosshair.png", pos = Vec3(0, 0, 0), scale = (0.05))
+        self.Hud.setTransparency(TransparencyAttrib.MAlpha)
+
+app = MyApp() # create instance of MyApp
+app.run() # run application
